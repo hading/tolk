dac29cb1c12025ddb96b4c40ea37bf0b714bbefe
Howard Ding <hding2@illinois.edu>
2013-03-15 11:59:57 -0500

    Trying to fix YAML error

--- lib/tolk/engine.rb
+++ lib/tolk/engine.rb
@@ -2,7 +2,7 @@ require 'rails'
 
 module Tolk
   class Engine < Rails::Engine
-    SafeYAML::OPTIONS[:default_mode] = :safe
+    SafeYAML::OPTIONS[:default_mode] = nil
     isolate_namespace Tolk
   end
 end
